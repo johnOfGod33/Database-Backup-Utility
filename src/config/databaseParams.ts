bb8f6f087cff53564c0c9ca7a02a4112fbb5f593
jean de dieu <jeandedieusessou@gmail.com>
2024-10-28 15:26:39 +0000

    remove getDatabaseParams function

--- src/config/databaseParams.ts
+++ src/config/databaseParams.ts
@@ -6,21 +6,3 @@ export interface DatabaseParams {
   database?: string;
   filename?: string;
 }
-
-export const getDatabaseParams = (
-  username?: string,
-  password?: string,
-  host?: string,
-  port?: number,
-  database?: string,
-  filename?: string
-): DatabaseParams => {
-  return {
-    username,
-    password,
-    host,
-    port,
-    database,
-    filename,
-  };
-};
