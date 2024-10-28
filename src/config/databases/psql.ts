30c3bdfa2198715470567174236305d35d79cec8
jean de dieu <jeandedieusessou@gmail.com>
2024-10-28 15:25:22 +0000

    implement postgresql class and connection function

---
+++ src/config/databases/psql.ts
@@ -0,0 +1,30 @@
+import { DatabaseParams } from "../databaseParams.js";
+import { Client } from "pg";
+import chalk from "chalk";
+
+export default class Psql {
+  private databaseParams: DatabaseParams;
+
+  constructor(params: DatabaseParams) {
+    this.databaseParams = params;
+  }
+
+  async connection() {
+    const uri = `postgresql://${this.databaseParams.username}:${this.databaseParams.password}@${this.databaseParams.host}:${this.databaseParams.port}/${this.databaseParams.database}`;
+
+    const client = new Client({ connectionString: uri });
+
+    try {
+      console.log(chalk.yellow(`Starting connection test to : ${uri}`));
+
+      await client.connect();
+
+      console.info(chalk.green("Test sucess"));
+    } catch (err: any) {
+      console.error(`Failed to connect to database`);
+      console.error(`Error : ${err.message}`);
+    } finally {
+      client.end();
+    }
+  }
+}
